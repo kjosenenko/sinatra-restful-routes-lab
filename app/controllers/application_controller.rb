d9cf3975d4cc0d67d2753ead736ae3de02fcf055
KJ Osenenko <karljosenenko@gmail.com>
2020-12-29 15:16:53 -0700

    commit tree 00db1552d1240df9f12dabf824e8e3f374a6ffa0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,52 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  # base url
+  get '/' do
+    redirect to "/recipes"
+  end
+
+  # index all recipes
+  get '/recipes' do
+    erb :index
+  end
+
+  # new recipe form
+  get '/recipes/new' do
+    erb :new
+  end
+
+  # create a new recipe
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  # show a recipe
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
+  # edit a recipe form
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  # edit a recipe form
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe = params
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  # delete a recipe
+  delete '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect to "/recipes"
+  end
 
 end
